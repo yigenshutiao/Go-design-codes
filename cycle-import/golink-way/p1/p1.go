81ed4e23cbc0243cc94536a63444e8d4471002eb
Xing Gao <18340825824@163.com>
2022-07-31 23:58:10 +0800

    补充 go:linkname的注释

--- cycle-import/golink-way/p1/p1.go
+++ cycle-import/golink-way/p1/p1.go
@@ -12,6 +12,15 @@ func (p *PP1) HelloFromP1() {
 }
 
 // p1 包中使用p2的私有方法，原文：https://golang.org/cmd/compile/#hdr-Compiler_Directives
+/*
+	This special directive does not apply to the Go code that follows it. Instead,
+	the //go:linkname directive instructs the compiler to use “importpath.name” as
+	the object file symbol name for the variable or function declared as “localname”
+	in the source code. If the “importpath.name” argument is omitted, the directive uses
+	the symbol's default object file symbol name and only has the effect of making the symbol
+	accessible to other packages. Because this directive can subvert the type system and package modularity,
+	it is only enabled in files that have imported "unsafe".
+*/
 //go:linkname helloFromP2 p2.helloFromP2
 func helloFromP2()
 
